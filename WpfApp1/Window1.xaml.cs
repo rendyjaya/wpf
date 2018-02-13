36226ca01570a403c9b11aae7027ac5893f98eaf
rendyjaya <rendy_jaya@hotmail.com>
2018-02-13 14:34:39 +0100

    Add files via upload

---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using WpfApp1.Views;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            filldata();
+
+        }
+        public void filldata()
+        {
+            string CmdString = string.Empty;
+
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Database1.mdf;Integrated Security=True");
+
+
+            using (SqlConnection con = conn)
+            {
+                CmdString = "SELECT * FROM Devices";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Devices");
+                sda.Fill(dt);
+                dtgrid.ItemsSource = dt.DefaultView;
+            }
+        }
+
+       
+    }
+}
